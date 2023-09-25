dd454cf1d427295ade30c8f2ad6f15b3616b81c5
dalvealinf <dalvaeal2021@alu.uct.cl>
2023-09-25 00:54:19 -0300

    Firebase Implementation

--- appcedid/lib/main.dart
+++ appcedid/lib/main.dart
@@ -3,8 +3,12 @@ import 'package:ti3/screens/home.dart';
 import 'package:ti3/screens/juaker.dart';
 import 'package:ti3/screens/nacho.dart';
 import 'package:ti3/screens/diego.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
