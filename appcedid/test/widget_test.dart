31745b47c8ab37a7ea2fa399918e822b1c9d8c37
dalvealinf <dalvaeal2021@alu.uct.cl>
2023-10-03 02:25:44 -0300

    Firebase Authentication Implementation with Google API

--- appcedid/test/widget_test.dart
+++ appcedid/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:appcedid/main.dart';
+import 'package:ti3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
