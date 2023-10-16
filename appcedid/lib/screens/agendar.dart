69331da9b9bff7956a6631e012efa1980e203d58
Joaquin Aguilar <111210035+Juaker1@users.noreply.github.com>
2023-10-16 00:09:40 -0300

    Invitación para evento en correo de asesor

--- appcedid/lib/screens/agendar.dart
+++ appcedid/lib/screens/agendar.dart
@@ -4,9 +4,15 @@ import 'package:googleapis_auth/auth_io.dart' as googleapis_auth;
 import 'package:googleapis/calendar/v3.dart' as gcal;
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 // ignore: must_be_immutable
-class AgendarPage extends StatelessWidget {
+class AgendarPage extends StatefulWidget {
+  @override
+  _AgendarPageState createState() => _AgendarPageState();
+}
+
+class _AgendarPageState extends State<AgendarPage> {
   List<String> dates = [
     'Lunes 08:00-18:00',
     'Martes 08:00-18:00',
@@ -14,6 +20,17 @@ class AgendarPage extends StatelessWidget {
     'Jueves 08:00-18:00',
     'Viernes 08:00-18:00'
   ];
+
+  Map<String, dynamic>? selectedAsesor;
+
+  Future<List<Map<String, dynamic>>> fetchAsesores() async {
+    final CollectionReference asesores =
+        FirebaseFirestore.instance.collection('Asesores');
+    final QuerySnapshot snapshot = await asesores.get();
+    return snapshot.docs
+        .map((doc) => doc.data() as Map<String, dynamic>)
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,21 +47,19 @@ class AgendarPage extends StatelessWidget {
             // Top section
             Expanded(
               flex: 2,
-              child: Row(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  CircleAvatar(
-                    radius: 100.0,
-                    backgroundColor: Colors.blue,
+                  Text(
+                    selectedAsesor != null
+                        ? '${selectedAsesor!['Nombre']} ${selectedAsesor!['Apellidos']}'
+                        : 'Asesor',
+                    style: TextStyle(fontSize: 24),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Asesor',
-                        style: TextStyle(fontSize: 24),
-                      ),
-                      Text('Especializacion'),
-                    ],
+                  Text(
+                    selectedAsesor != null
+                        ? selectedAsesor!['Especialidad']
+                        : 'Especializacion',
                   ),
                 ],
               ),
@@ -53,18 +68,52 @@ class AgendarPage extends StatelessWidget {
             // Middle section
             Expanded(
               flex: 3,
-              child: Wrap(
-                spacing: 8.0, // gap between adjacent chips
-                runSpacing: 4.0, // gap between lines
-                children: List.generate(10, (index) {
-                  return Chip(
-                    avatar: CircleAvatar(
-                      radius: 30.0,
-                      backgroundColor: Colors.blue,
-                    ),
-                    label: Text('Asesor ${index + 1}'),
-                  );
-                }).toList(),
+              child: FutureBuilder<List<Map<String, dynamic>>>(
+                future: fetchAsesores(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                    return Text('No Asesores found.');
+                  } else {
+                    final asesores = snapshot.data!;
+                    return LayoutBuilder(
+                      builder: (context, constraints) {
+                        double widthPerItem = 150.0;
+                        double spacing = 8.0;
+                        int itemsPerRow =
+                            (constraints.maxWidth / (widthPerItem + spacing))
+                                .floor();
+
+                        return GridView.builder(
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: itemsPerRow,
+                            childAspectRatio: 3,
+                            crossAxisSpacing: spacing,
+                            mainAxisSpacing: spacing,
+                          ),
+                          itemCount: asesores.length,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  selectedAsesor = asesores[index];
+                                });
+                              },
+                              child: Chip(
+                                label: Text(
+                                    '${asesores[index]['Nombre']} ${asesores[index]['Apellidos']}'),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
               ),
             ),
             Divider(),
@@ -87,7 +136,24 @@ class AgendarPage extends StatelessWidget {
                   // Button
                   ElevatedButton(
                     onPressed: () {
-                      Navigator.pushNamed(context, '/calendario');
+                      if (selectedAsesor != null) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => PagCalendario(
+                              nombre: selectedAsesor!['Nombre'],
+                              apellidos: selectedAsesor!['Apellidos'],
+                              correo: selectedAsesor!['Correo'],
+                            ),
+                          ),
+                        );
+                      } else {
+                        // Optionally, show a message to the user to select an asesor first
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                              content: Text('Por favor seleccione un Asesor')),
+                        );
+                      }
                     },
                     child: Text('Agendar Hora'),
                   ),
@@ -102,6 +168,12 @@ class AgendarPage extends StatelessWidget {
 }
 
 class PagCalendario extends StatefulWidget {
+  final String nombre;
+  final String apellidos;
+  final String correo;
+
+  PagCalendario(
+      {required this.nombre, required this.apellidos, required this.correo});
   @override
   _PagCalendarioState createState() => _PagCalendarioState();
 }
@@ -110,12 +182,50 @@ class _PagCalendarioState extends State<PagCalendario> {
   DateTime? _date;
   Duration _duration = Duration(minutes: 15);
   final currentUser = FirebaseAuth.instance.currentUser;
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final GoogleSignIn googleSignIn = GoogleSignIn(
     scopes: [
       'email',
       'https://www.googleapis.com/auth/calendar',
     ],
   );
+  //Check if another event is being stored at the same time
+  Future<bool> isOverlapping(DateTime start, DateTime end) async {
+    // Query for events that start before the end of the new event
+    QuerySnapshot querySnapshot1 = await FirebaseFirestore.instance
+        .collection('reunion')
+        .where('start', isLessThanOrEqualTo: end.toIso8601String())
+        .get();
+
+    // Query for events that end after the start of the new event
+    QuerySnapshot querySnapshot2 = await FirebaseFirestore.instance
+        .collection('reunion')
+        .where('end', isGreaterThanOrEqualTo: start.toIso8601String())
+        .get();
+
+    // Combine the results of the two queries
+    List<QueryDocumentSnapshot> combinedQuery = [];
+    combinedQuery.addAll(querySnapshot1.docs);
+    combinedQuery.addAll(querySnapshot2.docs);
+
+    // Check if any of the combined results overlap with the new event
+    for (var doc in combinedQuery) {
+      DateTime eventStart = DateTime.parse(doc['start']);
+      DateTime eventEnd = DateTime.parse(doc['end']);
+
+      if ((start.isAfter(eventStart) && start.isBefore(eventEnd)) ||
+          (end.isAfter(eventStart) && end.isBefore(eventEnd)) ||
+          (start.isAtSameMomentAs(eventStart) &&
+              end.isAtSameMomentAs(eventEnd)) ||
+          (start.isAtSameMomentAs(eventStart) && end.isAfter(eventStart)) ||
+          (start.isBefore(eventEnd) && end.isAtSameMomentAs(eventEnd))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -133,12 +243,8 @@ class _PagCalendarioState extends State<PagCalendario> {
               flex: 2,
               child: Column(
                 children: [
-                  CircleAvatar(
-                    radius: 69.0,
-                    backgroundColor: Colors.blue,
-                  ),
                   Text(
-                    'Reunirse con ese Asesor',
+                    'Reunirse con ${widget.nombre} ${widget.apellidos}',
                     style: TextStyle(fontSize: 24),
                   ),
                 ],
@@ -275,10 +381,51 @@ class _PagCalendarioState extends State<PagCalendario> {
                       ..timeZone = 'America/Santiago';
                     final calendar = gcal.CalendarApi(authenticatedClient);
                     final event = gcal.Event()
-                      ..summary = 'Reunión con Asesor'
+                      ..summary =
+                          'Reunión con ${widget.nombre} ${widget.apellidos}'
                       ..start = start
                       ..end = end;
-                    await calendar.events.insert(event, 'primary');
+
+                    if (_date != null) {
+                      bool isOverlap =
+                          await isOverlapping(_date!, _date!.add(_duration));
+                      if (!isOverlap) {
+                        // Add the event to Firestore
+                        event.attendees = [
+                          gcal.EventAttendee(
+                              email: widget
+                                  .correo), // Add the asesor's email as an attendee
+                        ];
+                        await calendar.events.insert(event, 'primary');
+
+                        // After successful insertion, add the event to Firestore
+                        await firestore.collection('reunion').add({
+                          'summary': event.summary,
+                          'start': event.start!.dateTime!.toIso8601String(),
+                          'end': event.end!.dateTime!.toIso8601String(),
+                        });
+                      } else {
+                        // Show a message that the event overlaps with an existing event
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text('Error'),
+                              content: Text(
+                                  'La hora seleccionada ya tiene una reunión agendada.'),
+                              actions: <Widget>[
+                                TextButton(
+                                  child: Text('Continuar'),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    }
                   }
                 }
               },
