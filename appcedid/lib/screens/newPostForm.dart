36e03ec80c104521a621d8dbc8156fcea20f897b
patoskixd <93287376+patoskixd@users.noreply.github.com>
2023-10-22 09:24:08 -0300

    arreglo form

--- appcedid/lib/screens/newPostForm.dart
+++ appcedid/lib/screens/newPostForm.dart
@@ -14,42 +14,87 @@ class _NewPostFormState extends State<NewPostForm> {
   final db =
       DatabaseService(); // Crea una instancia del servicio de base de datos aquí
 
+  InputDecoration _inputDecoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      filled: true,
+      fillColor: Colors.white,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blueGrey, width: 1.0),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.blue, width: 2.0),
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Nueva publicacion foro'),
+        title:
+            Text('Crea tu publicacion', style: TextStyle(color: Colors.black)),
+        iconTheme: IconThemeData(color: Colors.black),
         backgroundColor: Color.fromARGB(255, 235, 250, 151),
       ),
       body: Padding(
-        padding: EdgeInsets.all(4),
+        padding: EdgeInsets.all(16),
         child: Card(
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
           child: Padding(
-            padding: EdgeInsets.all(4),
+            padding: EdgeInsets.all(16),
             child: Form(
               key: _formKey,
               child: Column(
                 children: <Widget>[
                   TextFormField(
-                    decoration: InputDecoration(labelText: 'Titulo'),
+                    decoration: _inputDecoration('Titulo'),
                     onSaved: (value) {
                       title = value ?? '';
                     },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Materia'),
-                    onSaved: (value) {
-                      subject = value ?? '';
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(labelText: 'Cuerpo'),
-                    onSaved: (value) {
-                      body = value ?? '';
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa un título';
+                      }
+                      return null;
                     },
                   ),
                   SizedBox(height: 16),
-                  ElevatedButton(
+                  TextFormField(
+                    decoration: _inputDecoration('Materia'),
+                    onSaved: (value) {
+                      subject = value ?? '';
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa una materia';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    decoration: _inputDecoration('Cuerpo'),
+                    onSaved: (value) {
+                      body = value ?? '';
+                    },
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Por favor ingresa su consulta';
+                      }
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 32),
+                  ElevatedButton.icon(
                     onPressed: () async {
                       if (_formKey.currentState?.validate() ?? false) {
                         _formKey.currentState?.save();
@@ -65,7 +110,17 @@ class _NewPostFormState extends State<NewPostForm> {
                         Navigator.pop(context);
                       }
                     },
-                    child: Text('Submit'),
+                    icon: Icon(Icons.send),
+                    label: Text('Enviar'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      onPrimary: Colors.white,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
                   ),
                 ],
               ),
--- appcedid/lib/screens/perfil.dart
+++ appcedid/lib/screens/perfil.dart
@@ -1,34 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-class PerfilPage extends StatefulWidget {
-  @override
-  _PerfilPageState createState() => _PerfilPageState();
-}
-
-class _PerfilPageState extends State<PerfilPage> {
-  CollectionReference _collectionRef =
-      FirebaseFirestore.instance.collection('Usuarios');
-  List usuarioData = [];
-
-  @override
-  void initState() {
-    super.initState();
-    getData().then((_) {
-      setState(() {});
-    });
-  }
-
-  Future<void> getData() async {
-    // Get docs from collection reference
-    QuerySnapshot querySnapshot = await _collectionRef.get();
-
-    // Get data from docs and convert map to List
-    usuarioData = querySnapshot.docs.map((doc) => doc.data()).toList();
-
-    print(usuarioData);
-  }
-
+class PerfilPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,120 +11,102 @@ class _PerfilPageState extends State<PerfilPage> {
         iconTheme: IconThemeData(color: Colors.black),
       ),
       body: SafeArea(
-        child: FutureBuilder(
-          future: getData(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text("Error: ${snapshot.error}"));
-            } else if (usuarioData.isEmpty) {
-              return Center(child: Text("No hay datos disponibles"));
-            } else {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 110.0,
-                    backgroundImage: AssetImage('assets/user1.png'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 110.0,
+              backgroundImage: AssetImage('assets/user.png'),
+            ),
+            Text(
+              'Nombre del Docente',
+              style: TextStyle(
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.person,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'Nombre Y Apellido',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
                   ),
-                  Text(
-                    'Nombre del Docente',
-                    style: TextStyle(
-                      fontSize: 40.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.cake,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '07-06-1988',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
                   ),
-                  Card(
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.person,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        usuarioData[0]['Nombre'],
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                        ),
-                      ),
-                    ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  '+56900001111',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
                   ),
-                  Card(
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.cake,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'ALOG',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                        ),
-                      ),
-                    ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'docente@uct.cl',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
                   ),
-                  Card(
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        'celular',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                        ),
-                      ),
-                    ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.lock,
+                  color: Colors.teal,
+                ),
+                title: Text(
+                  'FideosConSalsa44232',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.teal.shade900,
                   ),
-                  Card(
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        usuarioData[0]['email'],
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Card(
-                    margin:
-                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.lock,
-                        color: Colors.teal,
-                      ),
-                      title: Text(
-                        usuarioData[0]['Clave'],
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.teal.shade900,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              );
-            }
-          },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
